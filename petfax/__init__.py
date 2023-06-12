2db623549ef9e0e506ae2a6efdd71d10dd84a36d
GreggLedoux <guitlouie@gmail.com>
2023-06-11 22:41:04 -0400

    petfax more

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,20 +1,22 @@
-from flask import Flask 
+# config                    
+from flask import Flask
 
-
+# factory
 def create_app():
-
     app = Flask(__name__)
 
-    from . import pet
+    # index route
+    @app.route('/')
+    def index(): 
+        return 'Hello, PetFax!'
+
+    # register pet blueprint 
+    from . import pet 
     app.register_blueprint(pet.bp)
 
+    # register fact blueprint 
+    from . import fact
+    app.register_blueprint(fact.bp)
 
-    @app.route('/')
-    def index():
-        return 'This is the home page'
-
-
-   
-    
-
+    # return the app 
     return app
